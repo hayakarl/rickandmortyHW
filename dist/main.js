bb0c2d930f472f64cd31a7bb7bbe6ddcbc480fd3
haya karl <khayakyony@gmail.com>
2024-02-20 15:02:03 +0200

    sdarot

---
+++ dist/main.js
@@ -0,0 +1,28 @@
+// <reference path="jquery-3.6.0.js" />
+"use strict";
+$(function () {
+    $("#ajaxButton").click(() => {
+        $("#loadingContainer").show();
+        $.ajax({
+            url: "https://rickandmortyapi.com/api/episode",
+            error: err => alert("Error: " + err.statusText),
+            success: sdarot => {
+                $("#loadingContainer").hide();
+                displaySdarot(sdarot);
+            }
+        });
+    });
+    function displaySdarot(sdarot) {
+        $("tbody").empty();
+        for (let i = 0; i < sdarot.length; i++) {
+            const tr = `
+                <tr>
+                    <td>${sdarot[i].id}</td>
+                    <td>${sdarot[i].name}</td>
+                    <td>${sdarot[i].air_date}</td>
+                    <td>${sdarot[i].episode}</td>
+                </tr>`;
+            $("tbody").append(tr);
+        }
+    }
+});
